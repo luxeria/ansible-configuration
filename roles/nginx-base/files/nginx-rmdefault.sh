36007f24a73d4a9e9946dfad7808a71ff46a926e
Sebastian Wicki <gandro@gmx.net>
2016-05-25 21:21:28 +0200

    Remove unmanaged config files

---
+++ roles/nginx-base/files/nginx-rmdefault.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+# Author: gandro
+# Reviewed: mindfuckup
+set -eu
+
+dpkg-query --showformat='${Conffiles}\n' --show 'nginx*' \
+    | grep '^ /etc/nginx' | while read file md5sum
+do
+    if echo "$md5sum" "$file" | md5sum --check --status 2>/dev/null ; then
+        echo "Removing ${file}"
+        rm -f "$file"
+    fi
+done
