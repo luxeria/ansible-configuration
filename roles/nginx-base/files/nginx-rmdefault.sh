2b423ce942d471460e589779a0821c9f2d10fc70
Sebastian Wicki <gandro@gmx.net>
2016-05-26 00:42:53 +0200

    nginx-base: Also remove unused directories and broken symlinks

--- roles/nginx-base/files/nginx-rmdefault.sh
+++ roles/nginx-base/files/nginx-rmdefault.sh
@@ -3,6 +3,7 @@
 # Reviewed: mindfuckup
 set -eu
 
+# Removes all unmodified config files in /etc/nginx
 dpkg-query --showformat='${Conffiles}\n' --show 'nginx*' \
     | grep '^ /etc/nginx' | while read file md5sum
 do
@@ -11,3 +12,10 @@ do
         rm -f "$file"
     fi
 done
+
+# Remove broken symlinks
+find /etc/nginx -type l -exec test ! -e {} \; -delete -printf "Removing %p\n"
+
+
+# Remove all empty directories
+find /etc/nginx -type d -empty -delete -printf "Removing %p\n"
